d787352c48b06828e7545cc94ccb0eaa5aa7200d
Ricardo <ricoazeredo@yahoo.com.br>
2022-11-15 18:27:05 -0300

    Atu config para hospedagem

--- config.php
+++ config.php
@@ -1,8 +1,20 @@
 <?php
+$status = 'produçao';
 
-$db_name = "estacionamento_db";
-$db_host = "localhost";
-$db_user= "root";
-$db_pass = "root";
+if($status=="local") {
+    //mac local
+    $db_name = "estacionamento_db";
+    $db_host = "localhost";
+    $db_user= "root";
+    $db_pass = "root";
+
+}else {
+    //em Produção
+    $db_name = "ricoaz56_estacionamento_db";
+    $db_host = "localhost";
+    $db_user= "ricoaz56_ricoaz56";
+    $db_pass = "1981Zpaneia@";
+}
 
 $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host,$db_user,$db_pass);
+
