ccc8c3f60c7662da1fd9abbecc88a77311f04eed
Ricardo <ricoazeredo@yahoo.com.br>
2022-11-15 18:48:57 -0300

    atu config

--- config.php
+++ config.php
@@ -8,12 +8,6 @@ if($status=="local") {
     $db_user= "root";
     $db_pass = "root";
 
-}else {
-    //em Produção
-    $db_name = "estacionamento_db";
-    $db_host = "localhost";
-    $db_user= "usuario";
-    $db_pass = "senha";
 }
 
 $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host,$db_user,$db_pass);
